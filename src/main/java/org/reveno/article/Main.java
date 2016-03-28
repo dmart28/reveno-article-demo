74edb732b04239439a54e5499b40799e3f2b583e
Artem Dmitriev <art.dm.ser@gmail.com>
2016-03-28 08:45:12 +0300

    update to version 1.22.1 + syntax sugar

--- src/main/java/org/reveno/article/Main.java
+++ src/main/java/org/reveno/article/Main.java
@@ -16,7 +16,7 @@ import static org.reveno.article.Utils.*;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         Reveno reveno = new Engine(args[0]);
         reveno.config().cpuConsumption(Configuration.CpuConsumption.HIGH);
 
@@ -24,7 +24,7 @@ public class Main {
         declareCommands(reveno);
 
         // Transaction actions declarations
-        declareTransactions(reveno);
+        declareTransactionActions(reveno);
 
         // Views mapping
         reveno.domain().viewMapper(TradeAccount.class, TradeAccountView.class, (id,e,r) ->
@@ -78,13 +78,21 @@ public class Main {
         // metrics.config().metricBufferSize(MeasureUtils.mb(2));
 
         // Object command = new ChangeBalance(accountId, 10);
+        // #### Warmup ####
+        // for (int i = 0; i < 10; i++) {
+        //     for (int j = 0; j < 1_000_000; j++) {
+        //         reveno.executeCommand(command);
+        //    }
+        // }
+        //
+        // #### Measurement ####
         // for (int i = 0; i < 45; i++) {
         //     for (int j = 0; j < 1_000_000; j++) {
         //         reveno.executeCommand(command);
         //    }
         // }
         //// just to see last results since metrics sends to sink every 15 seconds by default.
-        // Thread.sleep(15_000);
+        // System.in.read();
 
         reveno.shutdown();
     }
@@ -92,9 +100,9 @@ public class Main {
     protected static void declareCommands(Reveno reveno) {
         reveno.domain().command(CreateAccount.class, long.class, (c, ctx) -> {
             long accountId = ctx.id(TradeAccount.class);
-            ctx.executeTransaction(new CreateAccount.CreateAccountAction(c, accountId));
+            ctx.executeTxAction(new CreateAccount.CreateAccountAction(c, accountId));
             if (c.initialBalance > 0) {
-                ctx.executeTransaction(new ChangeBalance(accountId, toLong(c.initialBalance)));
+                ctx.executeTxAction(new ChangeBalance(accountId, toLong(c.initialBalance)));
             }
             return accountId;
         });
@@ -104,7 +112,7 @@ public class Main {
                 if (c.amount < 0 && account.balance < Math.abs(c.amount)) {
                     throw new IllegalArgumentException("Can't withdraw from account - not enough money.");
                 }
-                ctx.executeTransaction(c);
+                ctx.executeTxAction(c);
             } else throw new RuntimeException("No account " + c.accountId + " found!");
         });
         reveno.domain().command(MakeOrder.class, long.class, (c, ctx) -> {
@@ -113,7 +121,7 @@ public class Main {
                     throw new RuntimeException("Not sufficient finance!");
                 }
                 long orderId = ctx.id(Order.class);
-                ctx.executeTransaction(new MakeOrder.MakeOrderAction(orderId, toLong(c.price), c));
+                ctx.executeTxAction(new MakeOrder.MakeOrderAction(orderId, toLong(c.price), c));
                 return orderId;
             } else {
                 throw new IllegalArgumentException("One of the order command arguments are not correct.");
@@ -121,7 +129,7 @@ public class Main {
         });
         reveno.domain().command(CancellOrder.class, (c, ctx) -> {
             if (ctx.repo().has(Order.class, c.orderId)) {
-                ctx.executeTransaction(c);
+                ctx.executeTxAction(c);
             } else {
                 throw new IllegalArgumentException("Order with id=" + c.orderId + " not found.");
             }
@@ -129,9 +137,9 @@ public class Main {
         reveno.domain().command(AdjustOrder.class, (c, ctx) -> {
             if (ctx.repo().has(Order.class, c.orderId)) {
                 if (c.newSize <= 0) {
-                    ctx.executeTransaction(new CancellOrder(c.orderId));
+                    ctx.executeTxAction(new CancellOrder(c.orderId));
                 } else {
-                    ctx.executeTransaction(new AdjustOrder.AdjustOrderAction(c, toLong(c.newPrice)));
+                    ctx.executeTxAction(new AdjustOrder.AdjustOrderAction(c, toLong(c.newPrice)));
                 }
             } else {
                 throw new IllegalArgumentException("Order with id=" + c.orderId + " not found.");
@@ -142,21 +150,20 @@ public class Main {
         reveno.domain().command(ExecuteOrder.class, (c, ctx) -> {
             if (ctx.repo().has(Order.class, c.orderId)) {
                 Order order = ctx.repo().get(Order.class, c.orderId);
-                ctx.executeTransaction(new ChangeBalance(order.accountId, -order.price));
-                ctx.executeTransaction(new CancellOrder(c.orderId));
+                ctx.executeTxAction(new ChangeBalance(order.accountId, -order.price));
+                ctx.executeTxAction(new CancellOrder(c.orderId));
             } else {
                 throw new IllegalArgumentException("Order with id=" + c.orderId + " not found.");
             }
         });
     }
 
-    protected static void declareTransactions(Reveno reveno) {
+    protected static void declareTransactionActions(Reveno reveno) {
         reveno.domain().transactionAction(CreateAccount.CreateAccountAction.class, (a, ctx) ->
                 ctx.repo().store(a.id, new TradeAccount(a.id, a.info.currency)));
 
         reveno.domain().transactionAction(ChangeBalance.class, (a, ctx) -> {
-                ctx.repo().store(a.accountId,
-                        ctx.repo().get(TradeAccount.class, a.accountId).addBalance(a.amount));
+                ctx.repo().remap(a.accountId, TradeAccount.class, (id, e) -> e.addBalance(a.amount));
                 // uncomment to check that events are published
                 //ctx.eventBus().publishEvent(new BalanceChangedEvent(a.accountId));
         });
@@ -164,17 +171,16 @@ public class Main {
         reveno.domain().transactionAction(MakeOrder.MakeOrderAction.class, (a, ctx) -> {
             Order order = new Order(a.id, a.command.accountId, a.command.symbol, a.command.size, a.price);
             ctx.repo().store(a.id, order);
-            ctx.repo().store(a.command.accountId, ctx.repo().get(TradeAccount.class, a.command.accountId).addOrder(a.id));
+            ctx.repo().remap(a.command.accountId, TradeAccount.class, (id, e) -> e.addOrder(a.id));
         });
 
         reveno.domain().transactionAction(CancellOrder.class, (a, ctx) -> {
             Order order = ctx.repo().remove(Order.class, a.orderId);
-            ctx.repo().store(order.accountId, ctx.repo().get(TradeAccount.class, order.accountId).removeOrder(a.orderId));
+            ctx.repo().remap(order.accountId, TradeAccount.class, (id, e) -> e.removeOrder(a.orderId));
         });
 
         reveno.domain().transactionAction(AdjustOrder.AdjustOrderAction.class, (a, ctx) ->
-                ctx.repo().store(a.cmd.orderId,
-                        ctx.repo().get(Order.class, a.cmd.orderId).adjust(a.cmd.newSize, a.newPrice)));
+                ctx.repo().remap(a.cmd.orderId, Order.class, (id, e) -> e.adjust(a.cmd.newSize, a.newPrice)));
     }
 
 }
